47f4b4d9d3cfeadd63f900d82957f911af5ca3f9
az_aka <oswin.kazaki@gmail.com>
2022-11-05 18:49:02 +0900

    フラッグ戦

---
+++ src/main/java/flag/flag/EventListner.java
@@ -0,0 +1,20 @@
+package flag.flag;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class EventListner implements Listener {
+    @EventHandler
+    public void onPlaceBlock(PlayerInteractEvent e){
+        Player p = e.getPlayer();
+        if(e.getClickedBlock().getType() == Material.MOSSY_COBBLESTONE)return;
+        p.sendMessage("cancel");
+        e.setCancelled(true);
+    }
+}
---
+++ src/main/java/flag/flag/Flag.java
@@ -0,0 +1,33 @@
+package flag.flag;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Flag extends JavaPlugin {
+    public static Flag plugin;
+    public static Flag instance;
+    public Flag(){instance = this;}
+    private int amount;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        instance = this;
+        amount = 0;
+        plugin = this;
+        Bukkit.getPluginManager().registerEvents(new EventListner(),this);
+        //getCommand("creeper").setExecutor(new AzCommand());
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    public static Flag getInstance(){return instance;}
+    public int getAmount(){return amount;}
+    public void setAmount(int amount){this.amount = amount;}
+    public static Flag getPlugin(){
+        return plugin;
+    }
+}
